c89ec2b02fcb25e0b9cb40ceee1bbbc08dd3f053
unknown <thduszbzb12@gmail.com>
2025-07-17 17:35:35 +0900

    map으로 완료

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Detail from './Detail';
+import App2 from './App2';
 
 
 function App() {
@@ -10,6 +11,29 @@ function App() {
   //use~~~ : 이랙트 내장함수 ( 리액트 훅 )
   // let [a,b] = ['a',20] -> 이렇게 만들면 각 각 a,b에 들어감
   const [title, setTitle] = useState('게시판');
+
+  const [board, setBoard] = useState([
+
+    {
+      title : 'React',
+      date : '2025-07-17',
+      like : 0
+    },
+
+    {
+      title : 'HTML',
+      date : '2025-07-16',
+      like : 0
+    },
+
+    {
+      title : 'CSS',
+      date : '2025-07-15',
+      like : 0
+    }
+
+  ]);
+
   const [boardTitle, setBoardTitle] = useState(['React','HTML','CSS']);
   const [like, setLike] = useState([0,0,0]);
 
@@ -17,6 +41,8 @@ function App() {
 
   const [num,setNum] = useState(0); // 몇 번째 게시글을 클릭한지 저장하는 용도
 
+  const [newTitle,setNewTitle] = useState(''); // input에 작성 한 내용이 저장됨 (글작성 제목)
+  
   // let arr = [1,2,3,4,5];
 
   function change() {
@@ -26,7 +52,7 @@ function App() {
   return (
     
     <div className="App">
-
+      <App2/>
       {/* {
         arr.map((data)=>{
           // console.log('map 테스트');
@@ -55,6 +81,8 @@ function App() {
         setTitle('상품목록');
       }}>제목바꾸기</button>
 
+    
+
       
 
       {
@@ -72,10 +100,22 @@ function App() {
                 setLike(_like);
               }}>좋아요</button>{like[i]}</h4>
               <p>2025-07-16</p>
+              <button onClick={()=>{
+                let _boardTitle = [...boardTitle];
+                _boardTitle.splice(i,1);
+                setBoardTitle(_boardTitle);
+
+                let _like = [...like];
+                _like.splice(i,1);
+                setLike(_like);
+                
+              }}>삭제</button>
             </div>
           )
         })
       }
+
+      {/* 배열.splish( 삭제 할 인덱스 시작번호, 삭제 할 개수 ) */}
 
       {/* <div className="list">
         <h4 onClick={()=>{
@@ -108,6 +148,28 @@ function App() {
       {/* && 연산자도 가능 -> show && <Detail /> */}
       {/* h4를 클릭하면 보이게 숙제 */}
 
+      {/* oninput은 바로 입력, onchange는 글작성을 눌러야 변경 */}
+      <input type="text" value={newTitle} onInput={(e)=>{
+        setNewTitle(e.target.value);
+      }}/> 
+      <button onClick={()=>{
+        if ( newTitle ==='' ){
+          alert('제목을 입력하셔야 합니다.');
+          return;
+        }
+
+        let _boardTitle = [...boardTitle]; // 새로 작성 된 제목을 추가
+        _boardTitle.push(newTitle);
+
+        setBoardTitle(_boardTitle)
+
+        let _like = [...like]; //뒤에 좋아요를 추가
+        _like.push(0);
+
+        setLike(_like)
+
+        setNewTitle('');
+      }}>글작성</button>
       
     </div>
   )
