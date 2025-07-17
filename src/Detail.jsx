0ffc5668e2004ae9f4f9a96a00668c19b6e8165a
unknown <thduszbzb12@gmail.com>
2025-07-17 12:06:47 +0900

    디테일 끝

--- src/Detail.jsx
+++ src/Detail.jsx
@@ -1,7 +1,16 @@
-function Detail() {
+function Detail({boardTitle, setBoardTitle, num}) {
+  
+ 
+
   return (
-    <div className="detail">
-        <h4>글제목</h4>
+    <div className="detail" >
+        <button onClick={()=>{
+          let _boardTitle =[...boardTitle];
+          _boardTitle[0] = "zzz";
+          setBoardTitle(_boardTitle)
+          
+        }}>제목 바꿔주는 버튼</button>
+        <h4>{boardTitle[num]}</h4>
         <p>날짜</p>
         <p>내용</p>
       </div>
